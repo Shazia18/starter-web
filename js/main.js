a3c2fb4a60e278862179fbae3bbf125e10c65751
Shazia <shazia.nustceme@gmail.com>
2019-01-02 10:58:11 +0500

    getelementbyId

--- js/main.js
+++ js/main.js
@@ -1,1 +1,11 @@
-
+let elem = document.getElementById("col4");
+elem.style.backgroundColor="green";
+let btnDetials = document.getElementsByClassName("btn-default");
+btnDetials.length="4rem";
+let cont = document.getElementsByClassName("container");
+cont.style.color="red";
+document.getElementsByTagName('input')[0].value = 5;
+let inputs = form.querySelectorAll('input') 
+inputs[inputs.length-1] 
+let container_2 = document.getElementsByClassName("btn-lg");
+container_2.style.backgroundColor="purple";
