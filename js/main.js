20e5f868e39acc52274b3d1b859d0048a475e56b
Shazia <shazia.nustceme@gmail.com>
2019-01-03 12:02:53 +0500

    errors removed

--- js/main.js
+++ js/main.js
@@ -1,11 +1,24 @@
 let elem = document.getElementById("col4");
-elem.style.backgroundColor="green";
+elem.style.backgroundColor="khaki";
 let btnDetials = document.getElementsByClassName("btn-default");
-btnDetials.length="4rem";
+btnDetials[0].textContent="View Detials here";
+btnDetials[0].style.backgroundColor="black";
+btnDetials[1].textContent="View Detials here";
+btnDetials[1].style.backgroundColor="black";
+btnDetials[2].textContent="View Detials here";
+btnDetials[2].style.backgroundColor="black";
 let cont = document.getElementsByClassName("container");
-cont.style.color="red";
+cont[0].style.height="2rem";
+cont[1].style.margin="1rem";
 document.getElementsByTagName('input')[0].value = 5;
-let inputs = form.querySelectorAll('input') 
+let inputs = document.querySelectorAll('input');
+console.log(inputs);
 inputs[inputs.length-1] 
 let container_2 = document.getElementsByClassName("btn-lg");
-container_2.style.backgroundColor="purple";
+container_2[0].style.backgroundColor="purple";
+let foot = document.getElementsByTagName("footer");
+console.log(foot);
+foot[0].style.textAlign="center";
+foot[0].style.backgroundColor="grey";
+foot[0].style.fontSize="2rem";
+
