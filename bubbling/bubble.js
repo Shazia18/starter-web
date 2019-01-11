a8abbbaae28394a33578ef2082d1b0b2fbf22ddc
Shazia <shazia.nustceme@gmail.com>
2019-01-11 13:06:04 +0500

    created nested divs to understand bubbling effect. and used stop propagation

---
+++ bubbling/bubble.js
@@ -0,0 +1,34 @@
+
+document.querySelector(".one").addEventListener("click",function(){
+    console.log("one");
+});
+document.querySelector(".two").addEventListener("click",function(){
+    console.log("two");
+});
+document.querySelector(".three").addEventListener("click",function(){
+    console.log("three");
+});
+document.querySelector(".four").addEventListener("click",function(){
+    console.log("four");
+});
+document.querySelector(".five").addEventListener("click",function(event){
+    console.log("five");
+    // event.stopPropagation();
+    // event.stopImmediatePropagation();
+    
+    if (document.getElementById("check").checked) {
+        event.stopPropagation();
+        // event.stopImmediatePropagation();
+        
+      }
+        
+    
+   
+});
+ document.querySelector(".five").addEventListener("click",function(event){
+     console.log("mouse over fifth div");
+    //  event.stopPropagation();
+    //  event.stopImmediatePropagation();
+   
+ });
+
