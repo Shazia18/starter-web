86b41f1948efea273bad4a3ec27b48c9362c7e71
Shazia <shazia.nustceme@gmail.com>
2019-01-18 11:05:54 +0500

    ES6 destructuring and for-each, for-in and for-of loop implemented

---
+++ Destructuringandes6loops/index.js
@@ -0,0 +1,35 @@
+// let {  name: username,  age: userage}= user;
+// console.log(username,userage);
+let user={  name: "Sara",  age: 24 ,score:{ math: 45, physics: 634}};
+
+let{ name: username,age: userage, dob= "1-1-199"}=user;
+console.log(username,userage,dob);
+let{ name, age,score: { math, physics,islamiat=80}}=user;
+console.log(math,physics,islamiat);
+let word=  [{name:"loren", Age: 2},
+  {name: "Brice", Age:5}];
+
+for(let value of word){
+  console.log(value);
+}
+const obj = {
+    a: 1,
+    b: {nickname: "nicki",lastName: "whatever"},
+    c: 3,
+    d: 4
+    }
+    for (let elem in obj) {
+    console.log( obj[elem] )
+    };
+
+const pets=[{ name: "tomy", age: 12, favoritefood: "dogfood"},
+{name:"jasper", age: 5, favoritefood: "catfood"},
+{ name: "miranda",age: 23, favoritefood:"grass"}];
+pets.forEach(function(index){
+    console.log(index.name);
+
+});
+// const arr = ['cat', 'dog', 'fish'];
+// arr.forEach(element => {
+// console.log(element);
+// });
--- es6oop/index.js
+++ es6oop/index.js
@@ -1,89 +1,103 @@
-class Car {
-    constructor(name, make, price, model, desc) {
-      this.name = name;
-      this.make = make;
-      this.model = model;
-      this.price = price;
-      this.desc = desc;
-    }
-    static display() {
-      const item = JSON.parse(localStorage.getItem("cars"));
-      // console.log(item);
-      if (item != null) {
-        for (let i = 0; item.length > i; i++) {
-          const list = document.getElementById("data");
-          const row = document.createElement("tr");
-          row.innerHTML = `
-            <td>${item[i].name} </td>
-            <td>${item[i].make} </td>
-            <td>${item[i].model} </td>
-            <td>${item[i].price}</td>
-            <td>${item[i].desc}</td>
-            <td><button class="cancel" value="${i}">[x]</button></td>
+// class Car {
+//     constructor(name, make, price, model, desc) {
+//       this.name = name;
+//       this.make = make;
+//       this.model = model;
+//       this.price = price;
+//       this.desc = desc;
+//     }
+//     static display() {
+//       const item = JSON.parse(localStorage.getItem("cars"));
+//       // console.log(item);
+//       if (item != null) {
+//         for (let i = 0; item.length > i; i++) {
+//           const list = document.getElementById("data");
+//           const row = document.createElement("tr");
+//           row.innerHTML = `
+//             <td>${item[i].name} </td>
+//             <td>${item[i].make} </td>
+//             <td>${item[i].model} </td>
+//             <td>${item[i].price}</td>
+//             <td>${item[i].desc}</td>
+//             <td><button class="cancel" value="${i}">[x]</button></td>
 
             
-          `;
-          list.appendChild(row);
-          console.log(list);
-        }
-      } else {
-        localStorage.setItem("cars", JSON.stringify([]));
-      }
+//           `;
+//           list.appendChild(row);
+//           console.log(list);
+//         }
+//       } else {
+//         localStorage.setItem("cars", JSON.stringify([]));
+//       }
 
-    }}
+//     }}
 
 
-  class Store extends Car {
-    constructor(name, make, price, model, desc) {
-      super(name, make, price, model, desc);
-    }
-    insertCars(car) {
-      console.log(car);
-      let cars;
-      if (localStorage.getItem("cars") === null) {
-        cars = [];
-      } else {
-        cars = JSON.parse(localStorage.getItem("cars"));
-      }
-      cars.push(car);
-      localStorage.setItem("cars", JSON.stringify(cars));
-    }
+//   class Store extends Car {
+//     constructor(name, make, price, model, desc) {
+//       super(name, make, price, model, desc);
+//     }
+//     insertCars(car) {
+//       console.log(car);
+//       let cars;
+//       if (localStorage.getItem("cars") === null) {
+//         cars = [];
+//       } else {
+//         cars = JSON.parse(localStorage.getItem("cars"));
+//       }
+//       cars.push(car);
+//       localStorage.setItem("cars", JSON.stringify(cars));
+//     }
   
-  }
-  Car.display();
+//   }
+//   Car.display();
 
-  document.body.addEventListener("submit", function(e) {
-    const name = document.getElementById("name").value;
-    // console.log(name);
-    const make = document.getElementById("make").value;
-    const price = document.getElementById("price").value;
-    const model = document.getElementById("model").value;
-    const desc = document.getElementById("desc").value;
-    // console.log(desc);
-    e.preventDefault();
-    const obj = new Store(name, make, price, model, desc);
-    console.log(obj);
-    obj.insertCars(obj);
-  });
-  function removeRow(){
-    let btn = document.getElementsByClassName("cancel");
-   document.body.addEventListener("click",function(e){
-     if(e.target.classList.contains("cancel")){
-       e.target.parentElement.parentElement.remove();
-     }
+//   document.body.addEventListener("submit", function(e) {
+//     const name = document.getElementById("name").value;
+//     // console.log(name);
+//     const make = document.getElementById("make").value;
+//     const price = document.getElementById("price").value;
+//     const model = document.getElementById("model").value;
+//     const desc = document.getElementById("desc").value;
+//     // console.log(desc);
+//     e.preventDefault();
+//     const obj = new Store(name, make, price, model, desc);
+//     console.log(obj);
+//     obj.insertCars(obj);
+//   });
+//   function removeRow(){
+//     let btn = document.getElementsByClassName("cancel");
+//    document.body.addEventListener("click",function(e){
+//      if(e.target.classList.contains("cancel")){
+//        e.target.parentElement.parentElement.remove();
+//      }
 
-   });
-   const item = JSON.parse(localStorage.getItem("cars"));
-      // console.log(item);
-      if (item != null) {
-        for (let i = 0; item.length > i; i++) {
-        if(i===btn.value){
-          item.splice(i,1);
-        }
+//    });
+//    const item = JSON.parse(localStorage.getItem("cars"));
+//       // console.log(item);
+//       if (item != null) {
+//         for (let i = 0; item.length > i; i++) {
+//         if(i===btn.value){
+//           item.splice(i,1);
+//         }
        
           
 
-        }
-      }
+//         }
+//       }
+//   }
+//   removeRow();
+
+let user ={
+  name: "Shazia",
+  age: "23",
+  score: {
+    math: 40,
+    physics: 20
   }
-  removeRow();
+
+
+
+}
+
+
